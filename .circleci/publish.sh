307b3f3051ceb5e335dfd13f2bc7baa6cd9565c3
Evan Sosenko <evan.sosenko@meltwater.com>
2017-10-25 00:08:20 -0700

    Always run prepublishOnly before publish

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -12,9 +12,9 @@ pkg_version=$(jq -r '.version' package.json)
 if [[ "$(git log -1 --pretty='%s')" == "${pkg_version}" ]]; then
   if [[ -z "$(npm view ${pkg_name}@${pkg_version})" ]]; then
     pkg_files=(*.tgz)
-    if [[ "$(npm -v)" =~ "^3\." ]]; then
+    # if [[ "$(npm -v)" =~ "^3\." ]]; then
       npm run prepublishOnly
-    fi
+    # fi
     npm publish $(echo $pkg_files)
     echo
     echo "> Published ${pkg_name}@${pkg_version}."
